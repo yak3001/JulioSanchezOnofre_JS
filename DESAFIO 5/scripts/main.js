aa5feda242d7e0782c0f5462d8ce058b58cac505
Julio Sanchez Onofre <juliocesar.sanchez@alum.upf.edu>
2021-10-18 10:33:18 -0500

    Actualización con clases

--- DESAFIO 5/scripts/main.js
+++ DESAFIO 5/scripts/main.js
@@ -1,49 +1,58 @@
-//FUNCION PARA CONSTRUIR OBJETO
+//CONSTRUCCION CLASE
 
-function ProcesoElectoral(presupuestoPrevio, presupuestoActual, presupuestoPartidos, votos, listaNominal) {
-    this.presupuestoPrevio = presupuestoPrevio;
-    this.presupuestoActual = presupuestoActual;
-    this.presupuestoPartidos = presupuestoPartidos;
-    this.votos;
-    this.listaNominal;
-    this.participacion = function (participacion) {
-        participacion = ((votos / listaNominal) * 100);
-        return participacion.toFixed(2);
+class ProcesoElectoral {
+    constructor(presupuestoPrevio, presupuestoActual, presupuestoPartidos, votos, listaNominal) {
+        this.presupuestoPrevio = presupuestoPrevio;
+        this.presupuestoActual = presupuestoActual;
+        this.presupuestoPartidos = presupuestoPartidos;
+        this.votos = votos;
+        this.listaNominal = listaNominal;
     }
 
-    this.costoVoto = function (costoVoto) {
-        costoVoto = (presupuestoPrevio + presupuestoActual + presupuestoPartidos) / votos;
-        return costoVoto.toFixed(2);
+    participacion() {
+        return this.calcParticipacion();
     }
+    calcParticipacion() {
+        return ((this.votos / this.listaNominal) * 100).toFixed(2);
+    }
+
+    presupuestoElecciones() {
+        return this.calcPresupuestoElecciones();
+    }
+
+    calcPresupuestoElecciones() {
+        return this.presupuestoPrevio + this.presupuestoActual + this.presupuestoPartidos;
+    }
+
+    costoVoto() {
+        return this.calcCostoVoto();
+    }
+
+    calcCostoVoto() {
+        return (this.calcPresupuestoElecciones() / this.votos).toFixed(2);
+    }
+
 }
 
-//CONSTRUCTOR DE OBJETO procesoElectoral2015
+
+//OBJETO procesoElectoral2015
 
 const procesoElectoral2015 = new ProcesoElectoral(
     539799052, 4196776135, 5355522828, 39864082, 83536377
 )
 
-procesoElectoral2015.participacion();
-procesoElectoral2015.costoVoto();
-
-//CONSTRUCTOR DE OBJETO procesoElectoral2018
+//OBJETO procesoElectoral2018
 
 const procesoElectoral2018 = new ProcesoElectoral(
     699534390, 7144961645, 4138727092, 56611027, 89250881
 )
 
-procesoElectoral2018.participacion();
-procesoElectoral2018.costoVoto();
 
-//CONSTRUCTOR DE OBJETO procesoElectoral2021
+//OBJETO procesoElectoral2021
 
 const procesoElectoral2021 = new ProcesoElectoral(
     479250873, 7861864517, 7226003636, 49151320, 93328771
 )
-
-procesoElectoral2021.participacion();
-procesoElectoral2021.costoVoto();
-
 
 //SIMULADOR
 
